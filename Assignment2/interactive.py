62ca6ef7ab63fc1217595d258537903298518383
tzuling <s91010011@gmail.com>
2023-04-20 21:07:46 +0800

    爛番茄影評系統Assignment2

---
+++ Assignment2/interactive.py
@@ -0,0 +1,17 @@
+"""
+File: interactive.py
+Name: 
+------------------------
+This file uses the function interactivePrompt
+from util.py to predict the reviews input by 
+users on Console. Remember to read the weights
+and build a Dict[str: float]
+"""
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
